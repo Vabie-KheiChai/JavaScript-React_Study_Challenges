1a55439b9f1357ef3f7277f4d803ce61de0c39c7
Vabie Khei Chai <vbiekhei@gmail.com>
2026-09-21 21:21:11 +0530

    created

---
+++ Day_1/Functions/app.js
@@ -0,0 +1,14 @@
+
+// Function Declaration
+/* function greet(){
+    console.log("Hello");
+}
+greet(); */
+
+// Function Parameters
+
+function greet(firstName, LastName){
+    console.log("Hello  " + firstName + " love " + LastName);
+}
+greet("Khei Chai" , " Merry ");
+greet("Kheichai ", "Everything about her");
---
+++ README.md
@@ -0,0 +1,10 @@
+JavaScript&React_Study_Challenges
+
+// Day_1
+
+1. Function  
+Functions perform an action.
+Functions are reusable code of block and 
+.recognize
+.reuse code 
+.avoid repetition
